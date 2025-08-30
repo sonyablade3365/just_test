8181bb875c81231cb2ee483bd9a267cf7041c4aa
Grigorii Ilchenko <ilchenkogr3865@gmail.com>
2025-08-30 14:37:55 +0300

    remove test_without_add

--- test_without_add.py
+++
@@ -1,5 +0,0 @@
-# from playwright.sync_api import sync_playwright
-# with sync_playwright() as p:
-#     browser = p.chromium.launch_persistent_context(user_data_dir= r"C:\Users\User\AppData\Local\Google\Chrome\User Data\Default\Extensions\cfhdojbkjhnklbpkdaibdccddilifddb", headless=False)
-#     page = browser.new_page()
-#     page.goto("https://news.sportbox.ru")
