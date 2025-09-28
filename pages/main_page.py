8bec520ec47050423bcb58f7aef73479e232aee9
Grigorii Ilchenko <ilchenkogr3865@gmail.com>
2025-09-28 14:53:52 +0300

    add sidebar in main page class

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,11 @@
 import re
 
 from playwright.sync_api import Page
-
-
 from pages.base_page import BasePage
+from components.sidebar_component import SideComponent
 
 class MainPage(BasePage):
     """Клас Главной страницы"""
     def __init__(self, page: Page):
         super().__init__(page)
+        self.sidebar = SideComponent(page)
